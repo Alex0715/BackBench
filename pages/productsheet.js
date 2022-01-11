0a88e72842bc42f8a31b3ae87f2df0bb1f2a617f
Alex0715 <82712049+Alex0715@users.noreply.github.com>
2022-01-11 16:19:12 +0530

    Update productsheet.js

--- pages/productsheet.js
+++ pages/productsheet.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import ProductFeed from '../components/ProductFeed'
+import Link from "next/link";
 
 export default function productsheet({products}) {
   return (
